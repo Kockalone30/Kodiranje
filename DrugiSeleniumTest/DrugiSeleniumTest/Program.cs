4aba744e59f6b794961e4ff5471cf344c01209c6
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-10 20:51:23 +0100

    Inic

---
+++ DrugiSeleniumTest/DrugiSeleniumTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugiSeleniumTest
+{
+    public class Program
+    {
+        IWebDriver drive = new ChromeDriver();
+        static void Main(string[] args)
+        {
+         
+
+                       
+        }
+        [SetUp]
+        public void Inicijalizacija()
+        {
+            drive.Navigate().GoToUrl("https://www.saucedemo.com/");
+            drive.Manage().Window.Maximize();
+        }
+        [Test]
+
+        public void Test1()
+
+        {
+            IWebElement userName = drive.FindElement(By.Id("user-name"));
+            userName.SendKeys("standard_user");
+
+            IWebElement password = drive.FindElement(By.Id("password"));
+            password.SendKeys("secret_sauce");
+
+            IWebElement loginButton = drive.FindElement(By.Id("login-button"));
+            loginButton.Submit();
+
+
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            drive.Close();
+        }
+    }
+}
---
+++ Selenium Prvi Test/Selenium Prvi Test/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SeleniumPrviTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+          IWebDriver driver=new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            IWebElement element = driver.FindElement(By.Name("q"));
+                element.SendKeys("Selenium");
+            driver.Close;
+
+            //IWebElement elementButton = driver.FindElement(By.Name("btnk"));
+            //elementButton.Click();
+        }
+    }
+}
